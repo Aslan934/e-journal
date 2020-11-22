9b7fc7eb1c5c36962fabeea02c7f9567b6e24a8c
Aslan Rustamov <aslan.rustemov777@gmail.com>
2020-11-23 02:20:42 +0400

    Fixed db errors

--- core/models.py
+++ core/models.py
@@ -2,14 +2,27 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 
-class TeacherProfileManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
-        if not email:
-            raise ValueError("User must have an email")
+class TeacherManager(BaseUserManager):
+    """Manager for user profiles"""
 
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("User must have an email address")
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
+
         user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create new superuser"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
         user.save(using=self._db)
         return user
 
@@ -27,6 +40,9 @@ class Groups(models.Model):
     date = models.DateTimeField()
     subjects = models.ManyToManyField(Subject)
 
+    def __str__(self):
+        return self.name
+
 
 class Student(models.Model):
     name = models.CharField(max_length=250)
@@ -41,7 +57,8 @@ class Teacher(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=200)
     subjects = models.ManyToManyField(Subject, blank=True)
     groups = models.ManyToManyField(Groups)
-    objects = TeacherProfileManager()
+    is_staff = models.BooleanField(default=False)
+    objects = TeacherManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name', ]
